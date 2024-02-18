6126848230d5c9c202b90c3d08add37c29480ed3
John <jakhonravshanov@gmail.com>
2024-02-18 11:58:43 +0900

    Type conversion 3rd homework added

--- script.js
+++ script.js
@@ -4,8 +4,15 @@
 // num % 2 !== 0 && console.log('Toq son');
 
 /* Type conversion 2nd homework*/
-let  result = 44;
-result >= 86 && result <= 98 && console.log('A'); 
-result >= 61 && result <= 85 && console.log('B'); 
-result >= 46 && result <= 60 && console.log('C'); 
-result <=45  && console.log('Failed'); 
+// let  result = 44;
+// result >= 86 && result <= 98 && console.log('A'); 
+// result >= 61 && result <= 85 && console.log('B'); 
+// result >= 46 && result <= 60 && console.log('C'); 
+// result <=45  && console.log('Failed'); 
+
+/* Type conversion 3rd homework*/
+let fruit = 'pomidor';
+fruit === 'olma' && console.log(10000);
+fruit === 'nok' && console.log(12000);
+fruit === 'tarvuz' && console.log(20000);
+fruit === 'pomidor' && console.log("unday meva yoq");
