7d87146ec3fc333e6d87b3631a4b71f322cad89e
John <jakhonravshanov@gmail.com>
2024-02-20 20:43:27 +0900

    if else, switch case, ternary 1st homework added

--- script.js
+++ script.js
@@ -17,4 +17,15 @@
 // fruit === 'tarvuz' && console.log(20000);
 // fruit === 'pomidor' && console.log("unday meva yoq");
 
+/* if else, switch case, ternary 1st homework*/
+let result = 100;
+if (result >= 86) {
+    console.log('A');
+} else if (result >= 61 && result <= 85) {
+    console.log('B');
+} else if (result >= 46 && result <= 60) {
+    console.log('C');
+} else {
+    console.log('Failed');
+}
 
