b2fd3e3084d396a3557a77aecdf61057a6393194
John <jakhonravshanov@gmail.com>
2024-02-23 17:49:45 +0900

    for loop homework added

--- script.js
+++ script.js
@@ -75,10 +75,49 @@
 
 /////////////////////////////////////////////////////////////////////////////////
 /* if else, switch case, ternary 5th homework */
-let login = '';
-let message = (login == 'Employee') ? 'Hello' : 
-              (login === 'director') ? 'Greetings' : 
-              (login === '') ? 'No login' : 
-              '';
+// let login = '';
+// let message = (login == 'Employee') ? 'Hello' : 
+//               (login === 'director') ? 'Greetings' : 
+//               (login === '') ? 'No login' : 
+//               '';
 
-console.log(message);
+// console.log(message);
+
+////////////////////////////////////////////////////////////////////////////////
+/* for loop 1st homework */
+// var sum = 5;
+// for(i = 1; i <= 5; i++) {
+//   if( i % 2 === 0) console.log(`juft son ${i}`);
+//   else console.log(`toq son ${i}`);
+// }
+
+//////////////////////////////////////////////////////////////////////////////
+/* for loop 2nd homework */
+// let str = 'WeBbraIN';
+// let input = 'WBIN';
+// let newString = '';
+// for (var i = 0; i < str.length; i++) {
+//     if (input.includes(str[i])) {
+//         newString += str[i];
+//     }
+// }
+// console.log(newString);
+
+
+/////////////////////////////////////////////////////////////////////////////////
+/* for loop 3th homework */
+// let str = 'WebBrainAcadAmya';
+// let newStr = '';
+// for(let i = 0; i < str.length; i++ ) {
+//         if(str[i] === 'a' || str[i] === 'A' ){
+//             newStr += str[i]
+//         }
+//     }
+//     console.log('A, a-', newStr.length);
+    
+///////////////////////////////////////////////////////////////////////////
+/*for loop 4th homework */
+// let str = 'fozil';
+// for(let i = str.length; i >= 0; i--) {
+//     console.log(str[i]);
+// }
