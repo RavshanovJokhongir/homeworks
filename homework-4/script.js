00911b3a0b4024f704ea49e9de13f1596cd603ba
John <jakhonravshanov@gmail.com>
2024-02-29 15:44:00 +0900

    string 2nd homework

--- homework-4/script.js
+++ homework-4/script.js
@@ -1,4 +1,13 @@
 /* string 1st homework */
-let str = 'FoziL';
-let ending = str.endsWith('f')
-console.log(ending);
+// let str = '    FoziL       ';
+// let ending = str.trim().endsWith('L');
+// console.log(ending); //output:true, else:false
+/////////////////////////////////////////
+
+/* string 2nd homework */
+let str1 = 'Web';
+let str2 = 'Brain';
+let replacingStr1 = str1.replace('Web', 'Brain');
+let replacingStr2 = str2.replace('Brain', 'Web');
+console.log(replacingStr1);
+console.log(replacingStr2);
