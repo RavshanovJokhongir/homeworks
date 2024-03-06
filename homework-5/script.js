95b694f7e2c668dc0b8fe73007d3fb47ba8cfe70
John <jakhonravshanov@gmail.com>
2024-03-06 22:30:30 +0900

    Function  Homework 2

--- homework-5/script.js
+++ homework-5/script.js
@@ -1,10 +1,19 @@
 
 /////// Homework 1 /////////////////////
-function mySqrt(number) {
-    return number ** 0.5;
-}
-let num1 = 25;
-let num2 = 144;
+// function mySqrt(number) {
+//     return number ** 0.5;
+// }
+// let num1 = 25;
+// let num2 = 144;
 
-console.log(`Square root of ${num1} is: ${mySqrt(num1)}`);
-console.log(`Square root of ${num2} is: ${mySqrt(num2)}`);
+// console.log(`Square root of ${num1} is: ${mySqrt(num1)}`);
+// console.log(`Square root of ${num2} is: ${mySqrt(num2)}`);
+
+////////////////////////////////////////////////
+
+////// Homework 2 ///////////////
+function myAbs(number) {
+    return number >= 0 ? number : -number;
+}
+console.log(myAbs(-10)); 
+console.log(myAbs(10)); 
