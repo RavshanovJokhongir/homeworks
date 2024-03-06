143e600676a339d8f3e218dac914baf209c43024
John <jakhonravshanov@gmail.com>
2024-03-06 23:01:23 +0900

    object homework 4

--- homework-6/script.js
+++ homework-6/script.js
@@ -21,9 +21,27 @@
 // alert(isEmpty(schedule));
 
 //////// Homework 3 /////////
-let salaries = { John: 100, Ann: 160, Pete: 130 };
-let sum = 0;
-for (let key in salaries) {
-  sum += salaries[key];
-}
-console.log(sum);
+// let salaries = { John: 100, Ann: 160, Pete: 130 };
+// let sum = 0;
+// for (let key in salaries) {
+//   sum += salaries[key];
+// }
+// console.log(sum);
+
+/////// Homework 4 ////////////
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+  let obj = {
+    width: 20,
+    height: 30,
+    length: 10
+  };
+  console.log("Before multiplying:", obj);
+  multiplyNumeric(obj);
+  console.log("After multiplying:", obj);
+  
