6c0dd854a2cde4f8f1e52c44880af9265e06dcaf
John <jakhonravshanov@gmail.com>
2024-03-06 22:56:44 +0900

    object homework 3

--- homework-6/script.js
+++ homework-6/script.js
@@ -1,8 +1,29 @@
 
 /////Homework 1 //////
-let user = {};
-user.name = 'John';
-user.surname = 'Smith';
-user.name = 'Pete';
-delete user.name;
-console.log(user);
+// let user = {};
+// user.name = 'John';
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// delete user.name;
+// console.log(user);
+
+
+/////// Homework 2 ///////////
+// let schedule = {};
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
+// alert(isEmpty(schedule)); 
+// schedule["8:30"] = "get up";
+// alert(isEmpty(schedule));
+
+//////// Homework 3 /////////
+let salaries = { John: 100, Ann: 160, Pete: 130 };
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+console.log(sum);
