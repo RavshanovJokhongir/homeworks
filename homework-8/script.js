c095b83493d2419681b55a47460d3ea609d5f621
John <jakhonravshanov@gmail.com>
2024-03-13 20:52:51 +0900

    array homework-2

--- homework-8/script.js
+++ homework-8/script.js
@@ -1,19 +1,33 @@
 
 /////////Homework-1/////////////////
-var arr = [
-    {id: 1, name: 'Fozil'},
-    {id: 1, name: 'Odil'},
-    {id: 1, name: 'Alibek'},
-    {id: 1, name: 'Elyor'}
-];
+// var arr = [
+//     {id: 1, name: 'Fozil'},
+//     {id: 1, name: 'Odil'},
+//     {id: 1, name: 'Alibek'},
+//     {id: 1, name: 'Elyor'}
+// ];
 
-function onSearch(text) {
-    text = text.toLowerCase();
-    var searchResult = arr.filter(function(item) {
-        var itemName = item.name.toLowerCase();
-        return itemName.includes(text);
-    });
+// function onSearch(text) {
+//     text = text.toLowerCase();
+//     var searchResult = arr.filter(function(item) {
+//         var itemName = item.name.toLowerCase();
+//         return itemName.includes(text);
+//     });
 
-    return searchResult;
+//     return searchResult;
+// }
+// console.log(onSearch('o'));
+
+
+//////////homework-2/////////////////////
+let str = 'AABB ACC';
+
+function findFirstDoubleLetter(str) {
+    for (let i = 0; i < str.length - 1; i++) {
+        if (str[i] === str[i + 1]) {
+            return str[i];
+        }
+    }
+    return null; 
 }
-console.log(onSearch('o'));
+console.log(findFirstDoubleLetter(str)); 
