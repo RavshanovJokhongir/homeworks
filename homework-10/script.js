154b5b15a6b3289fd588679985db41586c3bbbd5
John <jakhonravshanov@gmail.com>
2024-03-20 20:39:03 +0900

    homework 2

--- homework-10/script.js
+++ homework-10/script.js
@@ -1,7 +1,25 @@
 //////// Homework 1 ///////////////////////////
-let obj={
-        id:1,
-        name:"WebBrain"
+// let obj={
+//         id:1,
+//         name:"WebBrain"
+//     }
+// let keys = Object.keys(obj);
+// console.log(keys); 
+
+//////// Homework 2 ///////////////////////////
+let obj = {
+    id: 1,
+    name: "WebBrain"
+};
+
+let Value = 1;
+let Key = null;
+
+for (let key in obj) {
+    if (obj[key] === Value) {
+        Key = key;
+        break; 
     }
-let keys = Object.keys(obj);
-console.log(keys); 
+}
+
+console.log(Key);
