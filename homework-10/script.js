c0c677113b11a9f6260b4814274dd51ffd494a1e
John <jakhonravshanov@gmail.com>
2024-03-23 18:00:05 +0900

    crud homework 4

--- homework-10/script.js
+++ homework-10/script.js
@@ -25,22 +25,39 @@
 // console.log(Key);
 
 ////////// Homework 3 //////////////////////////////
-let obj={
-        id:1,
-        name:"WebBrain",
-        offline:true,
-        online:true,
-        individual:false
-    }
-    let result = {};
-    for (let key in obj) {
-        if (typeof obj[key] === 'boolean' && obj[key] === true) {
-            result[key] = true;
-        }
-        if (typeof obj[key] === 'boolean' && obj[key] === false) {
-            result[key] = true;
-        }
-    }
+// let obj={
+//         id:1,
+//         name:"WebBrain",
+//         offline:true,
+//         online:true,
+//         individual:false
+//     }
+//     let result = {};
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'boolean' && obj[key] === true) {
+//             result[key] = true;
+//         }
+//         if (typeof obj[key] === 'boolean' && obj[key] === false) {
+//             result[key] = true;
+//         }
+//     }
     
-    console.log(result);
+//     console.log(result);
 
+///////// Homework 4 //////////////////////////////////
+person = {
+    id: 'Umar',
+    name: 'Ismoilov',
+    ielts: 5.5,
+    student: true,
+    parol:101
+}
+
+function Kirish(parol, name) {
+    if (parol === person,parol && name === person.name) {
+        console.log('saxifangizga xush kelibsiz!');
+    }else {
+        console.log('parol yoki name xato!');
+    }
+}
+Kirish(101, 'Umarxon')
