909ac07885806f48bab86c34ed2524d3326e896f
John <jakhonravshanov@gmail.com>
2024-03-23 17:59:34 +0900

    crud homework 3

--- homework-10/script.js
+++ homework-10/script.js
@@ -7,19 +7,40 @@
 // console.log(keys); 
 
 //////// Homework 2 ///////////////////////////
-let obj = {
-    id: 1,
-    name: "WebBrain"
-};
+// let obj = {
+//     id: 1,
+//     name: "WebBrain"
+// };
 
-let Value = 1;
-let Key = null;
+// let Value = 1;
+// let Key = null;
 
-for (let key in obj) {
-    if (obj[key] === Value) {
-        Key = key;
-        break; 
+// for (let key in obj) {
+//     if (obj[key] === Value) {
+//         Key = key;
+//         break; 
+//     }
+// }
+
+// console.log(Key);
+
+////////// Homework 3 //////////////////////////////
+let obj={
+        id:1,
+        name:"WebBrain",
+        offline:true,
+        online:true,
+        individual:false
     }
-}
+    let result = {};
+    for (let key in obj) {
+        if (typeof obj[key] === 'boolean' && obj[key] === true) {
+            result[key] = true;
+        }
+        if (typeof obj[key] === 'boolean' && obj[key] === false) {
+            result[key] = true;
+        }
+    }
+    
+    console.log(result);
 
-console.log(Key);
