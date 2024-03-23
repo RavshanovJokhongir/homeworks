6d0e098f9428d5cd8ad40ce1b53b07b73fe4ebed
John <jakhonravshanov@gmail.com>
2024-03-23 18:27:59 +0900

    recursion function homework 2

--- homework-11/script.js
+++ homework-11/script.js
@@ -1,7 +1,31 @@
 /////////// homework 1 /////////////
-const findSum = (n) => {
-    return (n * (n + 1)) / 2;
-  }
+// const findSum = (n) => {
+//     return (n * (n + 1)) / 2;
+//   }
   
-  console.log(findSum(5)); 
-  console.log(findSum(6));
+//   console.log(findSum(5)); 
+//   console.log(findSum(6));
+
+
+/////////// homework 2 ////////////////
+let obj = {
+    age: 100,
+    child: {
+        age: 50,
+        child: {
+            age: 20
+        }
+    }
+}
+const sumAges = (obj) => {
+    let sum = 0;
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            sum += sumAges(obj[key]); 
+        } else if (key === 'age') {
+            sum += obj[key];
+        }
+    }
+    return sum;
+};
+console.log(sumAges(obj));
